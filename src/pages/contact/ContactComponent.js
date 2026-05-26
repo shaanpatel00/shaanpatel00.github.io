4c52c8a49de5261055639c16f3bddd2c5ecb3af8
shaanpatel00 <spatel3050@gatech.edu>
2026-05-26 13:44:03 -0400

    Remove commented-out Blogs and Address sections from ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -4,8 +4,8 @@ import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import BlogsImg from "./BlogsImg";
-import AddressImg from "./AddressImg";
+// import BlogsImg from "./BlogsImg";
+// import AddressImg from "./AddressImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
@@ -55,6 +55,7 @@ class Contact extends Component {
               </div>
             </div>
           </Fade>
+          {/* Blogs and Address Sections Removed
           <Fade bottom duration={1000} distance="40px">
             <div className="blog-heading-div">
               <div className="blog-heading-text-div">
@@ -77,10 +78,10 @@ class Contact extends Component {
                 </div>
               </div>
               <div className="blog-heading-img-div">
-                {/* <img
+                <img
 											src={require(`../../assests/images/${blogSection["avatar_image_path"]}`)}
 											alt=""
-										/> */}
+										/>
                 <BlogsImg theme={theme} />
               </div>
             </div>
@@ -88,10 +89,10 @@ class Contact extends Component {
           <Fade bottom duration={1000} distance="40px">
             <div className="address-heading-div">
               <div className="contact-heading-img-div">
-                {/* <img
+                <img
 											src={require(`../../assests/images/${addressSection["avatar_image_path"]}`)}
 											alt=""
-										/> */}
+										/>
                 <AddressImg theme={theme} />
               </div>
               <div className="address-heading-text-div">
@@ -130,6 +131,7 @@ class Contact extends Component {
               </div>
             </div>
           </Fade>
+          */}
         </div>
         <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
         <TopButton theme={this.props.theme} />
