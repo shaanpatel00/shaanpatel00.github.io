fa2c8bffc99043b91022c6cb669266922b5494b9
shaanpatel00 <spatel3050@gatech.edu>
2026-05-26 16:17:32 -0400

    Add CI environment variable to build step and clean up commented code in ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -11,9 +11,13 @@ import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
+{
+  /*}
 const blogSection = contactPageData.blogSection;
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
+*/
+}
 
 class Contact extends Component {
   render() {
