674aff0fe7785ea1a131ac0dd44a5b6e7d370085
shaanpatel00 <spatel3050@gatech.edu>
2026-05-26 16:39:39 -0400

    Update deploy-pages action to version 5 and clean up commented code in ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -11,13 +11,11 @@ import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-{
-  /*}
+/*
 const blogSection = contactPageData.blogSection;
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 */
-}
 
 class Contact extends Component {
   render() {
